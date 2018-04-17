236e342ceda4469be481d973c407253ba3e4f47b
himanshutyagi36 <tyagi.himanshu90@gmail.com>
2018-04-16 21:22:16 -0400

    training data creted

--- create_training_data.py
+++ create_training_data.py
@@ -14,27 +14,31 @@ for timeframe in timeframes:
     counter = 0
     test_done = False
     while cur_length == limit:
-        df = pd.read_sql("SELECT * FROM parent_reply WHERE unix > {} AND parent NOT NULL AND score > 0 ORDER BY unix ASC LIMIT {}".format(last_unix, limit), connection)
-        last_unix = df.tail(1).values[0]
+
+        df = pd.read_sql("SELECT * FROM parent_reply WHERE unix > {} and parent NOT NULL and score > 0 ORDER BY unix ASC LIMIT {}".format(last_unix,limit),connection)
+        last_unix = df.tail(1)['unix'].values[0]
         cur_length = len(df)
+
         if not test_done:
-            with open("test.from", 'a', encoding='utf8') as f:
+            with open('test.from','a', encoding='utf8') as f:
                 for content in df['parent'].values:
                     f.write(content+'\n')
-            with open("test.to",'a', encoding='utf8') as f:
+
+            with open('test.to','a', encoding='utf8') as f:
                 for content in df['comment'].values:
-                    f.write(content+'\n')
+                    f.write(str(content)+'\n')
 
             test_done = True
-        
+
         else:
-            with open("train.from", 'a', encoding='utf8') as f:
+            with open('train.from','a', encoding='utf8') as f:
                 for content in df['parent'].values:
                     f.write(content+'\n')
-            with open("train.to",'a', encoding='utf8') as f:
-                for content in df['comment'].values:
-                    f.write(content+'\n')
 
-        counter +=1
-        if counter % 20 ==0:
-            print (counter*limit,'rows completed so far')
+            with open('train.to','a', encoding='utf8') as f:
+                for content in df['comment'].values:
+                    f.write(str(content)+'\n')
+
+        counter += 1
+        if counter % 20 == 0:
+            print(counter*limit,'rows completed so far')
